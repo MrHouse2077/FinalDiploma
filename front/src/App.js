1927f0e82938e6f3800eda2ed0923deb278904c7
Ninelisss <ninelisss@mail.ru>
2022-10-26 00:21:37 +0300

    добавление модального окна поиска

--- front/src/App.js
+++ front/src/App.js
@@ -36,53 +36,53 @@ function App() {
 
   return (
     <div className="App">
-      <div className='wrap'>
-        <header>
+      <header>
 
-          <div className='header_fon'>
-            <ImageGallery items={images} />
-          </div>
+        <div className='header_fon'>
+          <ImageGallery items={images} />
+        </div>
+        
+
+
+        <div className='header_content'>
           
+          <img src="/logo.png" className='logo'/>
+          <SiteInfo/>
 
 
-          <div className='header_content'>
-            
-            <img src="/logo.png" className='logo'/>
-            <SiteInfo/>
+          <MainNav/>
+        </div>
 
+      </header>
 
-            <MainNav/>
-          </div>
+      <section className='content'>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="shop" element={<Shop />}>
 
-        </header>
+            {/* <Route
+              path="messages"
+              element={<category1/>}
+            /> */}
+            {/* <Route
+              path="messages"
+              element={<category2/>}
+            /> */}
 
-        <section className='content'>
-          <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="shop" element={<Shop />}>
-
-              {/* <Route
-                path="messages"
-                element={<category1/>}
-              /> */}
-              {/* <Route
-                path="messages"
-                element={<category2/>}
-              /> */}
-
-            </Route>
-            <Route path="blog" element={<Blog />} />
-            <Route path="contacts" element={<Contacts />} />
-          </Routes>
-          
-        </section> 
+          </Route>
+          <Route path="blog" element={<Blog />} />
+          <Route path="contacts" element={<Contacts />} />
+        </Routes>
         <div className='search'>
           <button onClick = {()=>setShow(true)}>
             Лупа
           </button>
           <SearchModal onClose={()=>setShow(false)} show = {show}/>
-        </div>
       </div>
+        
+      </section> 
+        
+      
     </div>
   );
 }
--- front/src/Components/MainNav/MainNav.jsx
+++ front/src/Components/MainNav/MainNav.jsx
@@ -1,6 +1,5 @@
 import Styles from "./MainNav.module.scss";
 import { NavLink } from 'react-router-dom';
-import classNames from 'classnames';
 
 function MainNav(){
     return(
