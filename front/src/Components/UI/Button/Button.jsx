8ce6fb2a5363a2884a47d70a227c73ce3f3c96b0
aleksandr <shurik.turashov@mail.ru>
2022-11-05 23:02:58 +0300

    валидатор универсальный сдела, опять накосячил не в той ветке сделал

--- front/src/Components/UI/Button/Button.jsx
+++ front/src/Components/UI/Button/Button.jsx
@@ -3,14 +3,12 @@ import Styles from "./Button.module.scss";
 
 function Button(props) {
     
-  let disabled = props.disabled
   let onClick = props.onClick
 
     return (
       <div className="Button">
         
         <button 
-          disabled = {disabled}
           onClick = {onClick}
         >
           {props.children}
