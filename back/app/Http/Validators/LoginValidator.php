0432402dc6835e63df25cc67cb28c60a183f1371
aleksandr <shurik.turashov@mail.ru>
2022-11-06 19:10:26 +0300

    нечего комитить

--- back/app/Http/Validators/LoginValidator.php
+++ back/app/Http/Validators/LoginValidator.php
@@ -10,21 +10,14 @@
         public static function loginCheck($request) {
             return Validator::make($request->all(),[
                                                         'email' => 'required|email:rfc,dns',
-<<<<<<< HEAD
-                                                        'password' => 'required|min:4',
-=======
                                                         'password' => 'required|min:3',
->>>>>>> 3d06e47a1287b5459cf39b745a8b8cc02c55e5cf
                                                     ],
                                                     [
                                                         'email.required' => 'Введите почту!',
                                                         'email.email:rfc,dns' => 'Введите почту!',
                                                         'password.required' => 'Введите пароль!',
-<<<<<<< HEAD
-                                                        'password.min:4' => 'Ошибка!',
-=======
                                                         'password.min:3' => 'Ошибка!',
->>>>>>> 3d06e47a1287b5459cf39b745a8b8cc02c55e5cf
+
                                                     ]
             );
         }
